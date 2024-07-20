8461191d05be95713affe651b288028df6cdf644
upendrakumar143 <upendrakumar7317483116@gmail.com>
2024-07-20 02:53:24 -0700

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,20 @@
+package snakegame;
+
+import javax.swing.*;
+
+public class SnakeGame extends JFrame {
+    
+    SnakeGame() {
+        super("Snake Game");
+        add(new Board());
+        pack();
+        
+        setSize(300, 300);
+        setLocationRelativeTo(null);
+        setResizable(true);
+    }
+
+    public static void main(String[] args) {
+        new SnakeGame().setVisible(true);
+    }
+}
